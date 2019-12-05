09e17ec773caa3f3565c2566314f3c6f17170f3c
Yanone <post@yanone.de>
2019-12-05 12:30:54 +0100

    Updated build script to produce an unhinted font

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 TTFDIR="./fonts"
 rm -r $TTFDIR/*.ttf
 
-fontmake -u ./sources/Girassol-Regular.ufo -o ttf --output-dir ./fonts/ -a
+fontmake -u ./sources/Girassol-Regular.ufo -o ttf --output-dir ./fonts/
 
 for f in $TTFDIR/*.ttf
 do
 	gftools fix-dsig --autofix $f
-	gftools fix-hinting $f
+	gftools fix-nonhinting $f $f.fix
 	mv $f.fix $f
+	rm ${f//.ttf/-backup-fonttools-prep-gasp.ttf}
 done
 
 # # Clean up
